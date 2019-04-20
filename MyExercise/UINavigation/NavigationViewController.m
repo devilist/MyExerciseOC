9e1e7cd6da82e9c4118d5df49e049220340da773
devilist <bit_zengpu@163.com>
2019-04-21 00:49:02 +0800

    这条记录是在appcode里提交的

--- MyExercise/UINavigation/NavigationViewController.m
+++ MyExercise/UINavigation/NavigationViewController.m
@@ -21,45 +21,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     CGRect screen = [[UIScreen mainScreen] bounds];
-    
+
     self.navigationItem.title = @"navigation";
     UIBarButtonItem *back =[[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleDone target:self action:@selector(onClick:)];
     back.tag = 100;
     self.navigationItem.leftBarButtonItem = back;
-    
-    
+
+
     CGFloat buttonW = screen.size.width, buttonH = 20, buttonTop = 150;
     UIButton *button1 = [UIButton buttonWithType:UIButtonTypeSystem];
     [button1 setTitle:@"scroll sample" forState:UIControlStateNormal];
     button1.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
     button1.frame = CGRectMake(40, buttonTop, buttonW, buttonH);
-    
+
     UIButton *button2 = [UIButton buttonWithType:UIButtonTypeSystem];
     [button2 setTitle:@"page sample" forState:UIControlStateNormal];
     button2.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
     button2.frame = CGRectMake(40, 180, buttonW, buttonH);
-    
+
     UIButton *button3 = [UIButton buttonWithType:UIButtonTypeSystem];
     [button3 setTitle:@"tab sample" forState:UIControlStateNormal];
     button3.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
     button3.frame = CGRectMake(40, 210, buttonW, buttonH);
-    
+
     UIButton *button4 = [UIButton buttonWithType:UIButtonTypeSystem];
     [button4 setTitle:@"tree sample" forState:UIControlStateNormal];
     button4.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
     button4.frame = CGRectMake(40, 240, buttonW, buttonH);
-    
+
     [self.view addSubview:button1];
     [self.view addSubview:button2];
     [self.view addSubview:button3];
     [self.view addSubview:button4];
-    
+
     button1.tag = 1;
     button2.tag = 2;
     button3.tag = 3;
     button4.tag = 4;
 
-    
+
     [button1 addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
     [button2 addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
     [button3 addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
